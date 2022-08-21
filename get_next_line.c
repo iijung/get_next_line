74b5110c8768adc2c1da9e5b8d704cedbe7df336
iijung <987kmj4@naver.com>
2022-08-21 21:48:35 +0900

    fix: fix null_check_error

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: minjungk <minjungk@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 05:46:00 by minjungk          #+#    #+#             */
-/*   Updated: 2022/08/21 10:43:29 by minjungk         ###   ########.fr       */
+/*   Updated: 2022/08/21 21:48:10 by minjungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char	*join(char *dst, char *src, size_t slen, int new_line)
+static char	*join(char *dst, char *src, size_t slen, int *err)
 {
 	char	*tmp;
 	size_t	dlen;
@@ -24,16 +24,18 @@ static char	*join(char *dst, char *src, size_t slen, int new_line)
 		dlen = ft_strlen(dst);
 	if (slen > ft_strlen(src))
 		slen = ft_strlen(src);
-	tmp = (char *)ft_calloc(dlen + slen + new_line + 1, sizeof(char));
+	tmp = (char *)ft_calloc(dlen + slen + 1, sizeof(char));
 	if (tmp == 0)
+	{
+		*err = 1;
+		free(dst);
 		return (0);
+	}
 	ft_memcpy(tmp, dst, dlen);
 	ft_memcpy(tmp + dlen, src, slen);
-	if (new_line)
-		tmp[dlen + slen] = '\n';
 	free(dst);
-	ft_memmove(src, src + slen + new_line, BUFFER_SIZE - slen - new_line);
-	ft_memset(src + BUFFER_SIZE - slen - new_line, 0, slen + new_line);
+	ft_memmove(src, src + slen, BUFFER_SIZE - slen);
+	ft_memset(src + BUFFER_SIZE - slen, 0, slen);
 	return (tmp);
 }
 
@@ -43,23 +45,24 @@ char	*get_next_line(int fd)
 	ssize_t		read_len;
 	ssize_t		idx;
 	char		*rtn;
+	int			err;
 
+	err = 0;
 	rtn = 0;
-	idx = 0;
+	idx = -1;
 	read_len = BUFFER_SIZE;
-	while (fd >= 0)
+	while (0 <= fd)
 	{
-		if (idx == BUFFER_SIZE)
+		if (++idx == BUFFER_SIZE)
 		{
-			rtn = join(rtn, read_buf, ft_strlen(read_buf), 0);
+			rtn = join(rtn, read_buf, ft_strlen(read_buf), &err);
 			read_len = read(fd, read_buf, BUFFER_SIZE);
-			if (read_len < 0)
+			if (read_len < 0 || err)
 				break ;
 			idx = 0;
 		}
 		if (idx == read_len || read_buf[idx] == '\n')
-			return (join(rtn, read_buf, idx, read_buf[idx] == '\n'));
-		++idx;
+			return (join(rtn, read_buf, idx + (read_buf[idx] == '\n'), &err));
 	}
 	free(rtn);
 	return (0);
